6aaeef6fedf6f81e0994a5f26a555d48c817d9b9
halurabiyyu <halurabiyyu@gmail.com>
2024-07-10 16:24:59 +0700

    feat: add project

--- src/assets/projects.jsx
+++ src/assets/projects.jsx
@@ -1,4 +1,5 @@
 import ProjectImg from './project-test-img.png';
+import MamCatering from './mam_catering.png';
 import SIRW03Thumbnail from './sirw_thumbnail.png';
 
 export default function projects() {
@@ -8,11 +9,11 @@ export default function projects() {
             img: SIRW03Thumbnail,
             desc: 'Sistem Informasi Rukun Warga 03 Mergosono berbasis aplikasi web. '
         },
-        // {
-        //     name: 'Project 2',
-        //     img: ProjectImg,
-        //     desc: 'lorem ipsum'
-        // }
+        {
+            name: 'Mam Catering',
+            img: MamCatering,
+            desc: 'Point of Sales web apps for catering'
+        }
     ];
 
     return data;
