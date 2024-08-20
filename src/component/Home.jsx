271eea40a8c48738bc9b14cecc0732377bf45c46
halurabiyyu <halurabiyyu@gmail.com>
2024-08-20 18:37:10 +0700

    prettier format

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -121,7 +121,11 @@ export default function Home() {
                 fugiat aliqua.
               </p>
               <div className="mt-10 flex items-center justify-start gap-x-6">
-                <a href="https://github.com/halurabiyyu" target="_blank" class="text-gray-400 hover:text-indigo-600">
+                <a
+                  href="https://github.com/halurabiyyu"
+                  target="_blank"
+                  class="text-gray-400 hover:text-indigo-600"
+                >
                   <svg
                     class="h-6 w-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
@@ -130,7 +134,10 @@ export default function Home() {
                     <path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.301 24 12c0-6.627-5.373-12-12-12z" />
                   </svg>
                 </a>
-                <a href="https://www.linkedin.com/in/halur-muhammad-abiyyu/" class="text-gray-400 hover:text-indigo-600">
+                <a
+                  href="https://www.linkedin.com/in/halur-muhammad-abiyyu/"
+                  class="text-gray-400 hover:text-indigo-600"
+                >
                   <svg
                     class="h-6 w-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +162,7 @@ export default function Home() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
